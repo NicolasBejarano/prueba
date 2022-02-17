adf89e0fe69de3044b26f3de779819201fb8f018
Juan Carlos Villalobos <floppa@gmail.com>
2022-02-17 15:42:31 -0500

    posicion vertical del brazo

---
+++ brazo.py
@@ -0,0 +1,7 @@
+motor1 = ("Arriba")
+motor2 = 1
+motor3 = 90
+if motor3 == 90:
+    print("posicion graudal optima, no hay necesidad de girar mas")
+if motor1 == ("Arriba"):
+    print("Mover abajo")
